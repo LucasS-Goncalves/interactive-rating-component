506afe94f36fc958744c8f765aa5e518e5f9de24
Lucas <122225674+LucasS-Goncalves@users.noreply.github.com>
2023-06-13 17:46:55 -0300

    renaming properties in scores.service.spec.ts

--- src/app/scores.service.spec.ts
+++ src/app/scores.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { NotesService } from './notes.service';
+import { ScoresService } from './scores.service';
 
-describe('NotesService', () => {
-  let service: NotesService;
+describe('ScoresService', () => {
+  let service: ScoresService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(NotesService);
+    service = TestBed.inject(ScoresService);
   });
 
   it('should be created', () => {
